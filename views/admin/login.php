cd6053358d2770ce8729c1fd28246e80a375a1c3
ThuyLienLuong <162897498+ThuyLienLuong@users.noreply.github.com>
2024-12-08 02:27:38 +0700

    Update login.php

--- views/admin/login.php
+++ views/admin/login.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+
+
 // Include model User.php
 require_once 'C:\laragon\www\CngheWEB\tlunews1\models\User.php';
 
